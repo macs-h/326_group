5d48bd48f56b054f95e659a724a430eea787b1d2
Thomas Adams <tomadams@oucs1512.otago.ac.nz>
2018-07-23 15:33:38 +1200

    Comments for sage

--- etude02/sage.py
+++ etude02/sage.py
@@ -1,11 +1,28 @@
+# E2 - Sage and Trelawney
+#
+# @author Tom Adams
+# @author Max Huang
+# @author Mitchie Maluschnig
+# @author Asher Statham
+#
+# @since 23 July 2018
+
 golden_lookup = {}
 silver_lookup = {}
 
+# Finds the golden hour based on Trelawney's rules.
+#
+# Input:    d - the day
+# Returns:  the value of the golden hour
 def golden(d):
+    
+    # If the golden hour for this day has already been found, get the value
     if d in golden_lookup.keys():
         return golden_lookup.get(d)
     
     h = silver(d, d)
+
+    # Ensure that the hour is in the afternoon (1 - 12)
     if h % 12 == 0:
         h = 12
     else:
@@ -14,13 +31,20 @@ def golden(d):
     golden_lookup[d] = h
     return h
 
+# Finds the silver hour based on Trelawney's rules.
+#
+# Input:    b - the bronze of the day
+#           c - the copper of the day
+# Returns:  the value of the silver hour
 def silver(b, c):
     if c == 1:
         return 1
 
+    # If the silver hour for this day has already been found, get the value
     if (b, c) in silver_lookup.keys():
         return silver_lookup.get((b, c))
 
+    # Otherwise, check each rule and see if it has been checked before or not
     if b == c:
         if (b, c - 1) in silver_lookup.keys():
             x = silver_lookup.get((b, c - 1))
@@ -50,6 +74,8 @@ def silver(b, c):
         return x + y
 
     return 0
+
+# MAIN
 
 while True:
     print(golden(int(input("Enter a day: "))))
