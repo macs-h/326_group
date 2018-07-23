beb2b86396fcf6892aace36641d2b175cd3ccc0e
Max Huang <git@maxhuang.nz>
2018-07-23 14:00:59 +1200

    Starting code for etude07 (Summit) added.

---
+++ etude07/SummitGame.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Michael Albert
+ */
+public class SummitGame {
+
+    public static void main(String[] args) {
+        ArrayList<Player> players = new ArrayList<>();
+
+        players.add(0, new HumanPlayer());
+        players.add(1, new RollPlayer());
+//        players.add(2, new RandomPlayer());
+//        players.add(3, new RollPlayer());
+//        players.add(4, new RandomPlayer());
+//        players.add(5, new RollPlayer());
+
+        Round r = new Round(players.toArray(new Player[0]));
+    }
+    
+}
