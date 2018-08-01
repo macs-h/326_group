e87e09f2a4c650b4821255833abf243adc343cb0
Mitchel Maluschnig <malmi062@student.otago.ac.nz>
2018-08-01 14:04:29 +1200

    working on 'off by 2' bug

--- etude08/floating_point.py
+++ etude08/floating_point.py
@@ -9,6 +9,8 @@
 
 # CONSTANTS
 
+from math import log, pow
+
 EXP_BIAS = {"s": 127, "d": 1023}
 EXP_SIZE = {"s": 8, "d": 11}
 FRAC_SIZE = {"s": 23, "d": 52}
@@ -25,8 +27,10 @@ def frac(input_frac, input_p, output_p):
 
 def exp(input_exp, input_p, output_p):
     size = EXP_SIZE.get(output_p)
-
-    output_exp = int(input_exp, 2) - 64 + EXP_BIAS.get(output_p)
+    hex_exp = pow(16, int(input_exp, 2) - 64)
+    hex_exp = int(log(hex_exp,2))
+    print(hex_exp)
+    output_exp = hex_exp + EXP_BIAS.get(output_p)
     output_exp = bin(output_exp)[2:].zfill(size)[:size]
 
     return output_exp
@@ -49,6 +53,6 @@ output_p = input("Precision (s for single, d for double): ")
 with open(input_file) as numbers:
     write_file = open(output_file, 'w')
     for number in numbers:
-        number_array = [number[0], number[1:8], number[9:]]
+        number_array = [number[0], number[1:8], number[8:]]
         convert(number_array, input_p, output_p, write_file)
     write_file.close()
