c48b54ee0408e9dbcc66902dae44dba889adaaae
AsherStatham <31836845+AsherStatham@users.noreply.github.com>
2018-09-10 13:42:11 +1200

    Started tree search

--- etude09/smoker/array_smokers.py
+++ etude09/smoker/array_smokers.py
@@ -1,4 +1,3 @@
-
 from copy import deepcopy
 import sys
 import numpy as np
@@ -30,10 +29,37 @@ class Node:
     def get_total(self):
         return sum(self.distances)
 
+path = []
+def pathfinder(x, y, grid):
+    global path
+    pair = []
+    print(path)
+    if x > len(grid[0]):
+        print("x=", x)
+        pathfinder(x-1, y+1, grid)
+    elif y > len(grid):
+        print("y=", y)
+        pathfinder(x+1, y-1, grid)
+    if x == len(grid[0]) and y == len(grid):
+        return
+    if grid[x+1][y] > grid[x][y+1]:
+        pair.append(x+1)
+        pair.append(y)
+        path.append(pair)
+        if grid[x+1][y] != None:
+            pathfinder(x+1, y, grid)
+    else:
+        pair.append(x)
+        pair.append(y+1)
+        path.append(pair)
+        if grid[x][y+1] != None:
+            pathfinder(x, y+1, grid)
 
 # MAIN
 
-lines = sys.stdin.readlines()
+file = open('new_input.txt', 'r')
+lines = file.readlines()
+#lines = sys.stdin.readlines()
 input_data = []
 for line in lines:
     if line != "\n":
@@ -48,18 +74,15 @@ for line in lines:
         smokers = input_data[1:]
 
         for row in range(int(input_data[0][0])):
-            print("> row", row)
             for col in range(int(input_data[0][1])):
-                print("col", col, end="")
                 min_dist = int(input_data[0][0]) + 100 # change later.
                 for smoker in smokers:
                     distance_to_smoker = abs(int(smoker[0]) - col) + abs(int(smoker[1]) - row)
                     if distance_to_smoker < min_dist:
                         min_dist = distance_to_smoker
                 grid_array[row][col] = int(min_dist)
-                print("\tmin dist: ", int(grid_array[row][col]))
 
             print()
         print(grid_array)
 
-
+        pathfinder(0, 0, grid_array)
