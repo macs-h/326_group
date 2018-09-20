3c220107bbb9b6b0466912c3a60c19842ab551c4
Mitchie Maluschnig <mitchie.maluschnig@gmail.com>
2018-09-20 17:34:18 +1200

    whiten comments for betaBottyMcBotFace(or whatever hes going to be called)

--- etude07/CombinedPlayer.java
+++ etude07/CombinedPlayer.java
@@ -3,16 +3,19 @@ import java.util.Collections;
 
 public class CombinedPlayer implements Player {
 
-    private ArrayList<Float> pointEstimates = new ArrayList<>();
-    private int numTurn = 1;
-    float bestEstimate = 18;
+    private int numTurn = 1;//scaling as the game goes on?
+    float bestEstimate = 18;//used when we hit a showdown
     
+    //our bot chooses an action based on certain threshold (self-centred) vales
+    //as well as taking into account what it estimates the opponents scores to
+    //be.
     public Action takeTurn(State s, int[] dice){
 
         //Self-centredBot
+        //this part calculated the min and max of our dice as well as
+        //the sum of our dice.
         int max = 0;
         int min = 6;
-        //numTurn++;
         for (int d : dice){
             if (d > max){
                 max = d;
@@ -21,25 +24,20 @@ public class CombinedPlayer implements Player {
                 min = d;
             }
         }
-        /*if (max < 3){
-          return Action.FOLD;
-          } else if (min < 3){
-          selfAction = Action.ROLL;
-          }*/
-
-        //GuessBot
-            
            
         int mySum = 0;
         ArrayList <Action> myAction = new ArrayList<>();
         for(int i : dice){
             mySum += i;
         }
-            
+        
+        //this acts as a baseline, if our dice are all 5's and 6's we showdown.
         if (min > 4){
             return Action.SHOWDOWN;
         }
-        
+        //GuessBot
+        //the guessBot tries to guess what other players hands are and make
+        //a decision based on that.
         for(Integer player : s.playersRemaining){
             
             int thisMax = 0;
@@ -48,9 +46,12 @@ public class CombinedPlayer implements Player {
             ArrayList<Integer> playersRolls = (s.getRolls(player));
             ArrayList<Integer> maxDice = new ArrayList<>(Collections.nCopies(5,0));
             int notTaken = 1;
+            //loops through the previous rolls of opponents
             for(Integer roll : playersRolls){
                 Integer arrayMin = Collections.min(maxDice);
                 if(roll > 0) {
+                    //if the dice was taken we use that to build up a hand that
+                    //approximates the opponents hand.
                     thisMin += roll;
                     thisMax += roll;
                     numRolls++;
@@ -58,6 +59,8 @@ public class CombinedPlayer implements Player {
                         maxDice.set(maxDice.indexOf(arrayMin),roll);
                     }
                 } else {
+                    //if the dice wasnt taken we know that they have no die 
+                    //under that number.
                     if ((-1)*roll > notTaken){
                         notTaken = (-1)*roll;
                     }
@@ -68,12 +71,18 @@ public class CombinedPlayer implements Player {
                     }
                 }
             }
+            //here we try to calculate an estimate of the opponents hand.
+            //if the opponent hasnt taken 5 rolls we assume for the min that
+            //the unknown dice are 1's(or the highest dice that they rejected),
+            //for the max we assume that the unknown dice are all 6 and then 
+            //we find an average based on that.
+            //TODO sometimes a dice might overrite a dice that we know about 
+            //but we assume that the new dice overrites an unknown dice.
             float estimate = 0;
             if(numRolls <= 5){
                 thisMin += (5 - numRolls)*notTaken;
                 thisMax += (6*(5-numRolls));
                 estimate = (float)(thisMax + thisMin)/2;
-                pointEstimates.add(estimate);
             }else{
                 for(int num : maxDice){
                     estimate += num;
@@ -82,22 +91,31 @@ public class CombinedPlayer implements Player {
             if (estimate < bestEstimate){
                 bestEstimate = estimate;
             }
-            System.out.println("known Rolls: " + playersRolls + 
-                               " minimum Points: " + thisMin + 
-                               " maximumPoints: " + thisMax +
-                               " Point Estimate: " + estimate +
-                               " My Points: " + mySum);
-                
+//            System.out.println("known Rolls: " + playersRolls + 
+//                               " minimum Points: " + thisMin + 
+//                               " maximumPoints: " + thisMax +
+//                               " Point Estimate: " + estimate +
+//                               " My Points: " + mySum);
+            
+            //here we choose, based on each opponents score, what action we 
+            //wnt to take. to do this we check if our score is less than or
+            //greater than the opponents score by a (possibly scaling) 
+            //percentage and votes for a particular action based on that.
             if(estimate*(0.8+(0.04*numTurn)) > mySum || mySum < thisMin || max < 3){
                return Action.FOLD;
             }else if(mySum*(0.8+(0.04*numTurn)) > estimate || mySum >= thisMax){
-                myAction.add(Action.SHOWDOWN);//Unless our hand is very good and we want to be greedy
-            }else { //this ensures that if ANYONE has a really good hand the bot will fold
+                myAction.add(Action.SHOWDOWN);
+                //maybe implement greedyness if our hand is well above anyone elses
+            }else {
                 myAction.add(Action.ROLL);
             }
             
         }
-        //System.out.println("numTurn = " + numTurn);
+        //the action we take is determined by the following priority heirachy:
+        // FOLD > ROLL > SHOWDOWN. Therefore if any of our opponents make us 
+        //want to fold, we immediately fold. otherwise if any of the opponents 
+        //want us to roll, we choose to roll. for us to choose to showdown
+        //the votes must be unanimous. 
         for (Action action : myAction){
             if (action == Action.ROLL){
                 return Action.ROLL;
@@ -105,36 +123,17 @@ public class CombinedPlayer implements Player {
         }
         return Action.SHOWDOWN;
     }
-    
+    //our bot chooses whether to stay or not based on the largest estimate of
+    //an opponents hand. this ensures that it will only stay in the showdown if 
+    //it thinks it will win or tie (still possibility of losing due to chance,
+    //its a gambler at heart).
     public Action actAtShowdown(State s, int[] dice){
-        /*int sum = 0;
-          Action selfAction = Action.STAY;
-          Action deducAction = Action.STAY;
-          for (int d : dice){
-          sum += d;
-          }
-          for (Float estimate : pointEstimates){
-          if (estimate*(0.9+(0.04*numTurn)) > sum){
-          deducAction = Action.EXIT;
-          }
-          }
-          if (sum < 15){
-          selfAction = Action.EXIT;
-          }
-          if (deducAction == Action.EXIT || selfAction == Action.EXIT){
-          return Action.EXIT;
-          }
-          return Action.STAY;*/
 
         int sum = 0;
         for (int d : dice){
             sum += d;
         }
-        /*if (sum < 20){
-            return Action.EXIT;
-        }
-        return Action.STAY;*/
-        //System.out.println(bestEstimate + " " + sum);
+        
         if(sum >= bestEstimate){
             return Action.STAY;
         }else{
