1a8ae35aa4f59285421f6e5045f93c334d98ada8
AsherStatham <31836845+AsherStatham@users.noreply.github.com>
2018-09-20 12:30:24 +1200

    Finished bot?

--- etude07/CombinedPlayer.java
+++ etude07/CombinedPlayer.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class CombinedPlayer implements Player {
 
     private ArrayList<Float> pointEstimates = new ArrayList<>();
     private int numTurn = 1;
-    float bestEstimate = 1000;
+    float bestEstimate = 18;
     
     public Action takeTurn(State s, int[] dice){
 
@@ -35,32 +36,41 @@ public class CombinedPlayer implements Player {
             mySum += i;
         }
             
-        if (min > 3){
+        if (min > 4){
             return Action.SHOWDOWN;
         }
         
         for(Integer player : s.playersRemaining){
+            
             int thisMax = 0;
             int thisMin = 0;
             int numRolls = 0;
             ArrayList<Integer> playersRolls = (s.getRolls(player));
-            int[] maxDice = new int[5];
+            ArrayList<Integer> maxDice = new ArrayList<>(Collections.nCopies(5,0));
+            int notTaken = 1;
             for(Integer roll : playersRolls){
+                Integer arrayMin = Collections.min(maxDice);
                 if(roll > 0) {
-                    for(int i = 0;i < maxDice.length;i++){
-                        if( roll > maxDice[i]){
-                            maxDice[i] = roll;
-                            break;
-                        }
-                    }
                     thisMin += roll;
                     thisMax += roll;
                     numRolls++;
+                    if(roll > arrayMin){
+                        maxDice.set(maxDice.indexOf(arrayMin),roll);
+                    }
+                } else {
+                    if ((-1)*roll > notTaken){
+                        notTaken = (-1)*roll;
+                    }
+                    if (arrayMin < (-1)*roll){
+                        while (Collections.min(maxDice) < (-1)*roll){
+                            maxDice.set(maxDice.indexOf(Collections.min(maxDice)),(-1)*roll);
+                        }
+                    }
                 }
             }
             float estimate = 0;
             if(numRolls <= 5){
-                thisMin += (5 - numRolls);
+                thisMin += (5 - numRolls)*notTaken;
                 thisMax += (6*(5-numRolls));
                 estimate = (float)(thisMax + thisMin)/2;
                 pointEstimates.add(estimate);
@@ -72,21 +82,22 @@ public class CombinedPlayer implements Player {
             if (estimate < bestEstimate){
                 bestEstimate = estimate;
             }
+            System.out.println("known Rolls: " + playersRolls + 
+                               " minimum Points: " + thisMin + 
+                               " maximumPoints: " + thisMax +
+                               " Point Estimate: " + estimate +
+                               " My Points: " + mySum);
                 
-            if(estimate*(0.8+(0.04*numTurn)) > mySum || max < 3){
+            if(estimate*(0.8+(0.04*numTurn)) > mySum || mySum < thisMin || max < 3){
                return Action.FOLD;
-            }else if(mySum*(0.8+(0.04*numTurn)) > estimate && min > 3){
+            }else if(mySum*(0.8+(0.04*numTurn)) > estimate || mySum >= thisMax){
                 myAction.add(Action.SHOWDOWN);//Unless our hand is very good and we want to be greedy
             }else { //this ensures that if ANYONE has a really good hand the bot will fold
                 myAction.add(Action.ROLL);
             }
-            /*System.out.println("known Rolls: " + playersRolls + 
-                               " minimum Points: " + thisMin + 
-                               " maximumPoints: " + thisMax +
-                               " Point Estimate: " + estimate +
-                               " My Points: " + mySum);*/
+            
         }
-        System.out.println("numTurn = " + numTurn);
+        //System.out.println("numTurn = " + numTurn);
         for (Action action : myAction){
             if (action == Action.ROLL){
                 return Action.ROLL;
@@ -123,7 +134,8 @@ public class CombinedPlayer implements Player {
             return Action.EXIT;
         }
         return Action.STAY;*/
-        if(sum > bestEstimate){
+        //System.out.println(bestEstimate + " " + sum);
+        if(sum >= bestEstimate){
             return Action.STAY;
         }else{
             return Action.EXIT;
