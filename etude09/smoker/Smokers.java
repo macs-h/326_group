eb9251d2887a3413d6ebff7cfc7634a796c81513
AsherStatham <31836845+AsherStatham@users.noreply.github.com>
2018-10-08 17:47:30 +1300

    Done?

--- etude09/smoker/Smokers.java
+++ etude09/smoker/Smokers.java
@@ -5,9 +5,13 @@ public class Smokers extends Node{
   private static int heuristic;
   private static LinkedList<Node> queue = new LinkedList<Node>();
   private static int x, y, path;
+  private static ArrayList<Integer> totalDist = new ArrayList<Integer>();
+  private static ArrayList<Node> nonSmokers = new ArrayList<Node>();
+    private static ArrayList<Boolean> checkList = new ArrayList<Boolean>();
+    private static ArrayList<Node> closed = new ArrayList<Node>();
   
   public static void main(String[]args) throws FileNotFoundException, IOException{
-    File file = new File(args[0]);
+      File file = new File(args[0]);
     Scanner scan = new Scanner(file);
     while (true){
       if (scan.hasNextLine()){
@@ -23,6 +27,8 @@ public class Smokers extends Node{
       }
       heuristic = x+y;
       scan.nextLine();
+        totalDist.clear();
+        nonSmokers.clear();
       while (scan.hasNextLine()){
         String line = scan.nextLine();
         if (!line.isEmpty()){
@@ -31,16 +37,39 @@ public class Smokers extends Node{
           int j = Integer.parseInt(coords[1]);
           for (Node node : smokers){
             if (node.getX() == i && node.getY() == j){
-              node.setBlocked();
+              node.setBlocked(true);
+              checkList.add(true);
               queue.add(node);
+              nonSmokers.add(node);
             }
           }
         } else {
           break;
         }
       }
+      printWorld(smokers);
       voronoi(smokers);
       printWorld(smokers);
+      pathfinder(smokers);
+      ArrayList<Integer> minimums = new ArrayList<Integer>();
+      int total = 0;
+      int finalMin = Integer.MAX_VALUE;
+      for (Node n : nonSmokers){
+          int min = Integer.MAX_VALUE;
+          for (Node node : closed){
+              if (Math.abs(node.getX() - n.getX()) + Math.abs(node.getY() - n.getY()) < min){
+                  min = Math.abs(node.getX() - n.getX()) + Math.abs(node.getY() - n.getY());
+              }
+          }
+          total += min;
+          if (min < finalMin){
+              finalMin = min;
+          }
+          minimums.add(min);
+      }
+      System.out.println("Minimum distance: " + finalMin);
+      System.out.println("Total dist: " + total);
+      checkList.clear();
       if (!scan.hasNextLine()){
         scan.close();
         return;
@@ -48,58 +77,123 @@ public class Smokers extends Node{
     }
   }
   
-  public static void voronoi(ArrayList<Node> smokers){
-    ArrayList<Node> temp = smokers;
-    while (queue.size() > 0){
+    public static void voronoi(ArrayList<Node> smokers){
+        ArrayList<Node> temp = smokers;
+        ArrayList<Node> nodes = new ArrayList<Node>();
+        ArrayList<LinkedList<Node>> queues = new ArrayList<LinkedList<Node>>();
+        for (Node n : nonSmokers){
+            LinkedList <Node> l = new LinkedList<Node>();
+            l.add(n);
+            queues.add(l);
+        }
+        boolean hasChanged = true;
+        while (hasChanged){
+            for (int i = 0; i < queues.size(); i++){
+                ArrayList<Node> tempList = new ArrayList<Node>();
+                boolean rollback = false;
+                while (checkList.get(i) && queues.get(i).size() > 0){
+                   
+                    Node currentNode = queues.get(i).remove();
+                    for (Node node : smokers){
+                        if (currentNode.getX()+1 == node.getX() && currentNode.getY() == node.getY() && !node.getBlocked()){
+                            tempList.add(node);
+                            node.setBlocked(true);
+                        }
+                        if (currentNode.getX()-1 == node.getX() && currentNode.getY() == node.getY() && !node.getBlocked()){
+                            tempList.add(node);
+                            node.setBlocked(true);
+                        }
+                        if (currentNode.getX() == node.getX() && currentNode.getY()+1 == node.getY() && !node.getBlocked()){
+                            tempList.add(node);
+                            node.setBlocked(true);
+                        }
+                        if (currentNode.getX() == node.getX() && currentNode.getY()-1 == node.getY() && !node.getBlocked()){
+                            tempList.add(node);
+                            node.setBlocked(true);
+                        }
+                    }
+                    if (!pathfinder(smokers)){
+                        for (Node node : tempList){
+                            node.setBlocked(false);
+                        }
+                        checkList.set(i, false);
+                    } else {
+                        rollback = true;
+                    }
+                }
+                if( rollback){
+                    queues.get(i).addAll(tempList);
+                }
+            }
+            for (boolean b : checkList){
+                if (b){
+                    hasChanged = true;
+                    break;
+                }
+                hasChanged = false;
+            }
+        }
+    /*while (queue.size() > 0){
       Node current = queue.remove();
+      int i = -1;
       for (Node node : smokers){
+          i++;
         if (node.getX() == current.getX()+1 && node.getY() == current.getY()){
           if (current.getX()+1 < x && !node.getBlocked()){
-            node.setBlocked();
-            if (!pathfinder(smokers)){
-              node.setBlocked();
-            } else {
-              queue.add(node);
+            node.setBlocked(true);
+            nodes.add(node);
             }
           }
-        }
-      }
-      for (Node node : smokers){
         if (node.getX() == current.getX()-1 && node.getY() == current.getY()){
           if (current.getX()-1 > -1 && !node.getBlocked()){
-            node.setBlocked();
-            if (!pathfinder(smokers)){
-              node.setBlocked();
-            } else {
-              queue.add(node);
-            }
+            node.setBlocked(true);
+              nodes.add(node);
+            
           }
         }
-      }
-      for (Node node : smokers){
         if (node.getX() == current.getX() && node.getY() == current.getY()+1){
           if (current.getY()+1 < y && !node.getBlocked()){
-            node.setBlocked();
-            if (!pathfinder(smokers)){
-              node.setBlocked();
-            } else {
-              queue.add(node);
-            }
+            node.setBlocked(true);
+              nodes.add(node);
+            
           }
         }
-      }
-      for (Node node : smokers){
         if (node.getX() == current.getX() && node.getY() == current.getY()-1){
           if (current.getY()-1 > -1 && !node.getBlocked()){
-            node.setBlocked();
-            if (!pathfinder(smokers)){
-              node.setBlocked();
-            } else {
-              queue.add(node);
-            }
+            node.setBlocked(true);
+              nodes.add(node);
+            
           }
         }
       }
+      if (!pathfinder(smokers)){
+        for (Node node : nodes){
+          if (node.getX() == current.getX()+1 && node.getY() == current.getY() && node.getBlocked()){
+            node.setBlocked(false);
+          }
+        }
+        for (Node node : nodes){
+          if (node.getX() == current.getX()-1 && node.getY() == current.getY() && node.getBlocked()){
+            node.setBlocked(false);
+          }
+        }
+        for (Node node : nodes){
+          if (node.getX() == current.getX() && node.getY() == current.getY()+1 && node.getBlocked()){
+            node.setBlocked(false);
+          }
+        }
+        for (Node node : nodes){
+          if (node.getX() == current.getX() && node.getY() == current.getY()-1 && node.getBlocked()){
+            node.setBlocked(false);
+          }
+        }
+      } else {
+          //System.out.print(current.getX() + " " + current.getY() + ": ");
+        while (nodes.size() > 0){
+          queue.add(nodes.remove(0));
+        }
+      }
+      nodes.clear();
     }
     for (Node node : smokers){
       for (Node n : temp){
@@ -110,14 +204,15 @@ public class Smokers extends Node{
         }
         }
       }
-    }
+      }*/
   }
   
+      
+  
   public static boolean pathfinder(ArrayList<Node> smokers){
-    path = 0;
     LinkedList<Node> open = new LinkedList<Node>();
-    ArrayList<Node> closed = new ArrayList<Node>();
     ArrayList<Node> successors = new ArrayList<Node>();
+    closed.clear();
     for (Node node : smokers){
       if (node.getX() == 0 && node.getY() == 0){
         if (!node.getBlocked()){
@@ -134,13 +229,14 @@ public class Smokers extends Node{
         current = open.get(0);
         int j = 0;
         for (int i = 1; i < open.size(); i++){
-          if (heuristic - (current.getX() + current.getY()) > heuristic - (open.get(i).getX() + open.get(i).getY())){
+          if (heuristic - (current.getX() + current.getY()) >= heuristic - (open.get(i).getX() + open.get(i).getY())){
             current = open.get(i);
             j = i;
           }
         }
         current = open.remove(j);
       }
+      
       for (Node node : smokers){
         if (node.getX() == current.getX()+1 && node.getY() == current.getY()){
           if (current.getX()+1 < x && !node.getBlocked()){
@@ -194,27 +290,37 @@ public class Smokers extends Node{
         }
       }
       for (Node successor : successors){
-        boolean found = false;
-        if (successor.getX() == x-1 && successor.getY() == y-1){
+          boolean found = false;
+          if (successor.getX() == x-1 && successor.getY() == y-1){
           return true;
         }
-        for (Node o : open){
+        /*for (Node o : open){
           if (heuristic - (o.getX() + o.getY()) < heuristic - (successor.getX() + successor.getY())){
             found = true;
             break;
           }
-        }
+          }*/
         for (Node c : closed){
-          if (heuristic - (c.getX() + c.getY()) < heuristic - (successor.getX() + successor.getY())){
+            if (c.getX() == successor.getX() && c.getY() == successor.getY()){
+                //if (heuristic - (c.getX() + c.getY()) < heuristic - (successor.getX() + successor.getY())){
             found = true;
             break;
           }
         }
+        
         if (found == false){
           open.add(successor);
         }
       }
-      closed.add(current);
+      boolean found = false;
+      for (Node c : closed){
+        if (c.getX() == current.getX() && c.getY() == current.getY()){
+          found = true;
+        }
+      }
+      if (found == false){
+          closed.add(current);
+      }
     }
     return false;
   }
@@ -232,5 +338,25 @@ public class Smokers extends Node{
       System.out.println();
     }
     System.out.println();
+  }
+  
+  public static void findDist(ArrayList<Node> smokers){
+    
+      for (Node nonSmoker : nonSmokers){
+          int min = Integer.MAX_VALUE;
+          for (Node smoker : smokers){
+              if (!smoker.getBlocked() && Math.abs(smoker.getX() - nonSmoker.getX()) + Math.abs(smoker.getY() - nonSmoker.getY()) < min){
+                  min = Math.abs(smoker.getX() - nonSmoker.getX()) + Math.abs(smoker.getY() - nonSmoker.getY());
+              }
+          }
+          totalDist.add(min);
+      }
+      Collections.sort(totalDist);
+      System.out.println("Minimum distance: " + totalDist.get(0));
+      int total = 0;
+      for (int t : totalDist){
+          total += t;
+      }
+      System.out.println("Total distance: " + total);
   }
 }
